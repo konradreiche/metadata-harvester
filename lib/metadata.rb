df0143e0ec8e8313820532edc833c89f2dc9b5f6
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-15 23:15:40 +0200

    Add recursive symbolize keys method
    The previous implementation of symbolizing the keys in a Hash did not
    work on nested Hash. Hence, I have added a better method turning every
    key into a symbol.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/metadata.rb
+++ lib/metadata.rb
@@ -1,11 +1,26 @@
+require 'rails'
+
 module Metadata
+
 
   class Dataset
 
     def initialize(attributes={})
-      @attributes = attributes
-      @attributes.each_pair do |name, value|
-        instance_variable_set :"@#{name}", value unless meta_metadata? name
+      @attributes = Dataset.recursive_symbolize_keys attributes
+    end
+
+    def self.recursive_symbolize_keys(h)
+      case h
+      when Hash
+        Hash[
+          h.map do |k, v|
+            [ k.respond_to?(:to_sym) ? k.to_sym : k, recursive_symbolize_keys(v) ]
+          end
+        ]
+      when Enumerable
+        h.map { |v| recursive_symbolize_keys(v) }
+      else
+        h
       end
     end
 
