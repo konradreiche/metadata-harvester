3997e2f0cc9454aff17fd9d04770e43b3a9f5c63
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-15 23:10:21 +0200

    Add logger and extended JSON parser
    For better standard output management I have added the Ruby logger to
    the harvester. Also there is a fundamental problem with the delivered
    metadata. Some properties are actually arrays or objects, but are
    encoded as strings.
    
    To overcome this difficulty I have prototyped a method which recursively
    applies JSON.parse.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -1,10 +1,13 @@
 require 'curb'
 require 'json'
+require 'logger'
 require 'tire'
 require 'yaml'
 
 require_relative 'ckan'
 
+$log = Logger.new(STDOUT)
+$log.level = Logger::DEBUG
 
 def symbolize(hash)
   hash.each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
@@ -20,7 +23,7 @@ def query_dataset(base_url, dataset)
   curl.url = base_url + '/rest/dataset/' + dataset
   curl.ssl_verify_peer = false
   curl.perform
-  symbolize JSON.parse(curl.body_str)
+  parse(curl.body_str)
 end
 
 def query_datasets(base_url) # rescue on wrong urls
@@ -29,6 +32,32 @@ def query_datasets(base_url) # rescue on wrong urls
   curl.ssl_verify_peer = false
   curl.perform
   JSON.parse(curl.body_str)
+end
+
+def parse(json)
+
+
+  if json.is_a? Hash
+    
+    json.each do |key, value|
+      json[key] = parse value
+    end
+
+  elsif json.is_a? Array
+
+    json.map { |item| parse item }
+
+  else
+
+    begin
+      result = JSON.parse json
+      parse result
+    rescue
+      json == "" ? nil : json
+    end
+
+  end
+
 end
 
 def index(dataset)
@@ -42,8 +71,10 @@ end
 def harvest_ckan_repositories
   for url in load_repositories[:CKAN]
     datasets = query_datasets url
-    for name in datasets
-      index(query_dataset url, name)
+    datasets.each_with_index do |name, i|
+      $log.info "#{i} of #{datasets.length}"
+      dataset = query_dataset url, name
+      index dataset
     end
   end
 end
