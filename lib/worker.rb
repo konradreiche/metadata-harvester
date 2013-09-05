ac5bccad6e005a33261f8f2b38b200ff71bbd65a
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-05 10:42:15 +0200

    Finish restructuring of the metadata harvester
    I think more or less the restructuring process of the metadata harvester
    is finished, although I probably need to apply a lot of bug fixes now.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/worker.rb
+++ lib/worker.rb
@@ -12,128 +12,88 @@ require_relative 'ckan'
 require_relative 'json_archiver'
 
 module MetadataHarvester
+  include ActionView::Helpers::DateHelper
 
   class Worker
     include Sidekiq::Worker
 
-    def download_dump(repository)
-      logger.info("Fetch dump from #{import}")
-      url = repository[:dump]
-      file_type = File.extname[1..-1]
-      case file_type
-      when :zip
-        zip_bytes = Curl.get(url).body_str
-        Zip::Archive.open_buffer(zip_bytes) do |zf|
-          # read the first file
-          zf.fopen(zf.get_name(0)) do |f|
-            unzipped = f.read
-            logger.info("Index records")
-            datasets = JSON.parse_recursively(unzipped)
-            @archiver.store(datasets)
-          end
-        end
-      when :gz
-        gz = Zlib::GzipReader.new(open(url))
-        index(parse(gz.read), source)
-      end
+    ##
+    # Initializes basic attributes.
+    #
+    def initialize
+      @timeout = 0
     end
 
-    def 
-
+    ##
+    # Starts the metadata harvesting procedure
+    #
     def perform(repository, options)
       @compress = compress
       @archiver = JsonArchiver.new(source) if @archive
-
       if repository.key?(:dump)
         download_dump(repository)
-      end
-
-      if import.nil?
-        steps = (count(url) / limit.to_f).ceil
-        logger.info("Index records")
-
-        before = Time.new
-        elapsed = 0
-        steps.times do |i|
-          datasets = query(url, limit, i)
-          index(datasets, source)
-          now = Time.new
-          e = (now - before) * (steps - i + 1)
-          display = ActionView::Base.new.distance_of_time_in_words(before, before + e)
-          logger.info("#{i} of #{steps} - #{url} ~ #{display}")
-          before = now
-        end
       else
+        download_records(repository)
       end
     end
 
+    ##
+    #  Downloads and extracts the dump of a repository.
+    #
+    def download_dump(repository)
+      url = repository[:dump]
+      type = File.extname[1..-1]
+      @archiver.download(url)
+      @archiver.extract(type)
+    end
+
+    ##
+    # Downloads the metadata records through the exposed API.
+    #
+    def download_records(repository)
+      url = repository[:url]
+      rows = repository[:rows]
+      steps = count(url).fdiv(limit).ceil
+
+      before = Time.new
+      steps.times do |i|
+        records = query(url, rows, i)
+        @archiver.store(records)
+        now = Time.new
+        elapsed = (now - before) * (steps - i + 1)
+        eta = distance_of_time_in_words(before, before + elapsed)
+        logger.info("#{i} of #{steps} - #{url} ~ #{eta}")
+        before = now
+      end
+    end
+
+    ##
+    # Retrieve the number of total metadata records.
+    #
     def count(url)
       curl = Curl::Easy.new
-      curl.url = url + '/search/dataset'
+      curl.url = "#{url}/search/dataset"
       curl.ssl_verify_peer = false
       curl.perform
       JSON.parse(curl.body_str)['count']
     end
 
-    def query(url, limit, i)
-      url = url + '/3/action/package_search'
-      data = { rows: limit, start: i }
+    ##
+    # Queries a limited number (+rows+) of metadata records from a repository.
+    #
+    # Uses the CKAN Search API.
+    #
+    def query(url, rows, i)
+      url = "#{url}/3/action/package_search"
+      data = { rows: rows, start: i }
       curl = Curl.get(url, data)
-      result = JSON.parse(curl.body_str)['result']['results']
-      result = result.each do |dataset|
-        dataset = parse dataset
-        dataset['extras'] = parse_extras dataset['extras']
-        dataset['groups'] = dataset['groups'].map { |g| g['name'] }
-        dataset['tags'] = dataset['tags'].map { |t| t['name'] }
-      end.compact
+      response = JSON.parse_recusively(curl.body_str)
+      result = response['result']['results']
     rescue JSON::ParserError, Curl::Err::PartialFileError => e
-      @sleep *= 2
+      @timeout *= 2
       logger.warn("Parse Error. Retry in #{sleep}s")
-      sleep @sleep
+      sleep(@timeout)
       retry
     end
 
-    def parse_extras(extras)
-      result = {}
-      extras.each do |item|
-        value = item['value']
-        if value.is_a? String
-          value[0] = "" if value[0] == '"'
-          value[value.length - 1] = "" if value[value.length - 1] == '"'
-          value.gsub!("\\","")
-        end
-        result[item['key']] = parse value
-      end
-      result
-    end
-
-    ##
-    #
-    def parse(json)
-      if json.is_a? Hash
-        json.each do |key, value|
-          json[key] = parse value
-        end
-      elsif json.is_a? Array
-        json.map { |item| parse item }
-      else
-        begin
-          result = JSON.parse json
-          parse result
-        rescue
-          json = nil if json.is_a? String and json.empty?
-          json = nil if json.is_a? Array and json.empty?
-          json = nil if json.is_a? Hash and json.empty?
-          json
-        end
-      end
-    end
-
-    def index(datasets, repository)
-      if @archive
-        @archiver.store(datasets)
-        return
-      end
-    end
-  end
 end
