a68c2929859c9813108b6331e18b186c18018470
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-06 16:26:26 +0200

    Implement repository selection option
    Now I have brought the repository selection option into effective. In
    the iteration the Sidekiq job creation is skipped if the selected
    repository and the current one do not match.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/mdh.rb
+++ lib/mdh.rb
@@ -5,12 +5,6 @@ require 'yaml'
 
 module MetadataHarvester
 
-  def self.load_repositories
-    path = File.expand_path('..', File.dirname(__FILE__))
-    result = YAML.load_file("#{path}/repositories.yml")
-    result.with_indifferent_access
-  end
-
   def self.start
     options = Trollop::options do
       opt :compress, 'Compresses the data with Gzip afterwards'
@@ -20,9 +14,23 @@ module MetadataHarvester
     catalog = load_repositories()
     catalog.each do |type, repositories|
       repositories.each do |repository|
+        next if skip?(options, repository)
         MetadataHarvester::Worker.perform_async(repository, options)
       end
     end
+  end
+
+  private
+  def self.load_repositories
+    path = File.expand_path('..', File.dirname(__FILE__))
+    result = YAML.load_file("#{path}/repositories.yml")
+    result.with_indifferent_access
+  end
+
+  def self.skip?(options, repository)
+    return false if options[:repository].nil?
+    selection = options[:repository]
+    selection.downcase != repository[:name].downcase
   end
 
 end
