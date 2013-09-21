16d105f30bb40ff4d13d205bb2094bc902a94405
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-21 02:17:06 +0200

    Add Yajl::Gzip stream reader and writer
    For reasons unknown the Yajl stream writer and reader for compression
    support are deprecated and will be removed with version 2.
    
    There was no commit since 2011. Since I want to avoid the deprecation
    message and because who knows if this ever becomes true, I have added
    the wrapper to my own code.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ lib/yajl_ext.rb
@@ -0,0 +1,35 @@
+module Yajl
+  module Gzip
+
+    class StreamReader < ::Zlib::GzipReader
+
+      def read(len=nil, buffer=nil)
+        if val = super(len)
+          unless buffer.nil?
+            buffer.replace(val)
+            return buffer
+          end
+          super(len)
+        else
+          nil
+        end
+      end
+
+      def self.parse(input, options={}, buffer_size=nil, &block)
+        if input.is_a?(String)
+          input = StringIO.new(input)
+        end
+        Yajl::Parser.new(options).parse(new(input), buffer_size, &block)
+      end
+
+    end
+
+    class StreamWriter < ::Zlib::GzipWriter
+
+      def self.encode(obj, io)
+        Yajl::Encoder.new.encode(obj, new(io))
+      end
+
+    end
+  end
+end
