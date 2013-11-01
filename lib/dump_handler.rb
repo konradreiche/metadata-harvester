871325e3277f900d6fae3c5aad75c9fb97e0c5d9
Konrad Reiche <konrad.reiche@gmail.com>
2013-11-01 05:36:07 +0100

    Do more experimentation on the Schandler parser
    I am still trying to figure out how to parse the dump into smaller
    pieces. Probably a specific handler can be written as a state machine.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/dump_handler.rb
+++ lib/dump_handler.rb
@@ -3,10 +3,33 @@ require 'oj'
 module MetadataHarvester
   class DumpHandler < Oj::ScHandler
 
-    def array_append(a, value)
+    attr_reader :record
+
+    def initialize
+      @stack = []
+      @close = false
+    end
+
+    def hash_start
+      @stack.push(Hash.new)
+    end
+
+    def hash_end
+      @close = true
     end
 
     def hash_set(h, key, value)
+      puts "#{key} => #{value}"
+      if @close
+        inner = @stack.pop
+        @stack.last[key] = inner
+      else
+        @stack.last[key] = value
+      end
+    end
+
+    def record
+      @stack.first
     end
 
   end
