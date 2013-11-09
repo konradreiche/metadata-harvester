7672ef32a9e7df53e24353fdc33bf0ee47982ed5
Konrad Reiche <konrad.reiche@gmail.com>
2013-11-09 02:55:20 +0100

    Start to write unit test for the unify method
    The unification method has been more complex, hence I add more tests to
    wire the specification.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ spec/lib/worker_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+require 'worker'
+require 'webmock/rspec'
+
+module MetadataHarvester
+
+  describe Worker do
+
+    subject { Worker.new }
+
+    describe "#count" do
+      it "counts the number of packages" do
+        url = 'http://www.example.com/api/action/package_search'
+        body = '{"result":{"count":1000}}'
+        stub_request(:post, url).with(body: '{}')
+        .to_return(status: 200, body: body, headers: {})
+       
+        response = subject.count('http://www.example.com/api')
+        expect(response).to be(1000)
+      end
+
+      it "counts the number of packages of a legacy repository" do
+        url = 'http://www.example.com/api/rest/dataset'
+        body = '["a", "b", "c"]'
+        stub_request(:get, url).to_return(status: 200, body: body, headers: {})
+
+        response = subject.count('http://www.example.com/api', true)
+        expect(response).to be(3)
+      end
+    end
+
+    describe "#unify" do
+      it "dismantles group objects to group strings" do
+        records = [{ 'tags'   => [], 
+                     'extras' => [],
+                     'groups' => [{ 'name' => 'finance' }, 
+                                  { 'name' => 'education' },
+                                  { 'name' => 'health' }]}]
+
+        result = subject.unify(records).first
+        expect(result['groups']).to eq(['finance', 'education', 'health'])
+      end
+
+      it "dismantles tag objects to tag strings" do
+        records = [{ 'groups' => [], 
+                     'extras' => [],
+                     'tags'   => [{ 'name' => 'economy' }, 
+                                  { 'name' => 'statistic' },
+                                  { 'name' => 'map' }]}]
+
+        result = subject.unify(records).first
+        expect(result['tags']).to eq(['economy', 'statistic', 'map'])
+      end
+
+      it "dismantles extras objects to an monolithic extras object" do
+        extras1 = { 'key' => 'language',  'value' => 'english' }
+        extras2 = { 'key' => 'harvested', 'value' => true }
+
+        records = [{ 'groups' => [], 
+                     'tags'   => [],
+                     'extras' => [extras1, extras2] }]
+
+        result = subject.unify(records).first
+        expectation = { 'language' => 'english', 'harvested' => true }
+        expect(result['extras']).to eq(expectation)
+      end
+    end
+
+  end
+
+end
