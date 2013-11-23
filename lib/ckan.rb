0202aac37009680adbb8b5d863894b22738b8cca
Konrad Reiche <konrad.reiche@gmail.com>
2013-11-23 23:44:10 +0100

    Optimize normalize extras method
    I have replaced the two JSON parse statements with just one. Not really
    an optimization, but it saves at least one parsing execution.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- lib/ckan.rb
+++ lib/ckan.rb
@@ -13,15 +13,17 @@ class CKAN
     elsif source.is_a?(Array)
       source.map { |item| normalize_extras(item) }
     elsif source.is_a?(String) and valid?(source)
-      normalize_extras(Oj.load(source))
+      normalize_extras(@value)
     else
       source
     end
   end
 
+  # Validates the source and stores the result for later use.
+  #
   def self.valid?(source)
-    value = Oj.load(source)
-    value.is_a?(Array) or value.is_a?(Hash) or value.is_a?(String)
+    @value = Oj.load(source)
+    @value.is_a?(Array) or @value.is_a?(Hash) or @value.is_a?(String)
   rescue Oj::ParseError
     false
   end
